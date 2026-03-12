68b39ebc08a9ca778ebda0bdd68d30d2b8970e29
wachiratony415-max <wachiratony415@gmail.com>
2026-03-12 12:37:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -162,5 +162,6 @@ with app.app_context():
 # -----------------------------
 if __name__ == "__main__":
     print("starting farm mind server.....")
-    app.run(host="0.0.0.0, port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
 
